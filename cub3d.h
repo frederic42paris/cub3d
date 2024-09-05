b522362b8a382d97d7704f40e8d4c07d7a7e6d04
Aether Guez <aet7gz@gmail.com>
2024-09-05 13:10:45 +0200

    refactor: move preprocessor instructions to header file to improve compiling times

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: arguez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 14:23:24 by arguez            #+#    #+#             */
-/*   Updated: 2024/09/02 16:18:50 by arguez           ###   ########.fr       */
+/*   Updated: 2024/09/05 13:09:17 by arguez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define CUB3D_H
 
 # include "./minilibx/mlx.h"
-# include "libft/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
@@ -30,11 +29,19 @@
 # define KEY_S	115
 # define KEY_D	100
 # define KEY_A	97
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+# define TEXTW 64
+# define TEXTH 64
 
 typedef struct s_mlx
 {
 	void	*mlx_p;
 	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
 	void	*win_ptr;
 	double	posX;
 	double	posY;
@@ -66,6 +73,52 @@ typedef struct s_mlx
 	int		drawStart;
 	int		drawEnd;
 	int		color;
+
+	void	*greystone;
+	int		*greystone_addr;
+	void	*bluestone;
+	int		*bluestone_addr;
+	void	*red;
+	int		*red_addr;
+	void	*wood;
+	int		*wood_addr;
+	int		texNum;
+	double	wallX;
+	int 	texX;
+	double 	step;
+	double	texPos;
+	int		texY;
+	int		bits_per_pixel_grey;
+	int		line_length_grey;
+	int		endian_grey;
+	int		bits_per_pixel_blue;
+	int		line_length_blue;
+	int		endian_blue;
+	int		bits_per_pixel_red;
+	int		line_length_red;
+	int		endian_red;
+	int		bits_per_pixel_wood;
+	int		line_length_wood;
+	int		endian_wood;
+
+	float	rayDirX0;
+	float	rayDirY0;
+	float	rayDirX1;
+	float	rayDirY1;
+	int		p;
+	float	posZ;
+	float	rowDistance;
+	float	floorStepX;
+	float	floorStepY;
+	float	floorX;
+	float	floorY;
+	int		cellX;
+	int		cellY;
+	int		tx;
+	int		ty;
+	int		checkerBoardPattern;
+	int		floorTexture;
+	int		ceilingTexture;
 }	t_mlx;
 
 #endif
--- main.c
+++ main.c
@@ -6,117 +6,11 @@
 /*   By: ftanon <ftanon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:58:17 by ftanon            #+#    #+#             */
-/*   Updated: 2024/09/04 18:59:52 by ftanon           ###   ########.fr       */
+/*   Updated: 2024/09/05 13:09:36 by arguez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./minilibx/mlx.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-#include <X11/X.h>
-#include <X11/keysym.h>
-
-#define SW 640
-#define SH 480
-#define MAPW 24
-#define MAPH 24
-#define KEY_W	119
-#define KEY_S	115
-#define KEY_D	100
-#define KEY_A	97
-#define KEY_LEFT 65361
-#define KEY_RIGHT 65363
-#define TEXTW 64
-#define TEXTH 64
-
-typedef struct s_mlx
-{
-	void	*mlx_p;
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-	void	*win_ptr;
-	double	posX;
-	double	posY;
-	double	dirX;
-	double	dirY;
-	double	planeX;
-	double	planeY;
-	double	time;
-	double	oldTime;
-	double	cameraX;
-	double	rayDirX;
-	double	rayDirY;
-	int		mapX;
-	int		mapY;
-	double	sideDistX;
-	double	sideDistY;
-	double	deltaDistX;
-	double	deltaDistY;
-	double	perpWallDist;
-	double	moveSpeed;
-	double	rotSpeed;
-	double	oldDirX;
-	double	oldPlaneX;
-	int		stepX;
-	int		stepY;
-	int		hit;
-	int		side;
-	int		lineHeight;
-	int		drawStart;
-	int		drawEnd;
-	int		color;
-
-	void	*greystone;
-	int		*greystone_addr;
-	void	*bluestone;
-	int		*bluestone_addr;
-	void	*red;
-	int		*red_addr;
-	void	*wood;
-	int		*wood_addr;
-	int		texNum;
-	double	wallX;
-	int 	texX;
-	double 	step;
-	double	texPos;
-	int		texY;
-	int		bits_per_pixel_grey;
-	int		line_length_grey;
-	int		endian_grey;
-	int		bits_per_pixel_blue;
-	int		line_length_blue;
-	int		endian_blue;
-	int		bits_per_pixel_red;
-	int		line_length_red;
-	int		endian_red;
-	int		bits_per_pixel_wood;
-	int		line_length_wood;
-	int		endian_wood;
-
-	float	rayDirX0;
-	float	rayDirY0;
-	float	rayDirX1;
-	float	rayDirY1;
-	int		p;
-	float	posZ;
-	float	rowDistance;
-	float	floorStepX;
-	float	floorStepY;
-	float	floorX;
-	float	floorY;
-	int		cellX;
-	int		cellY;
-	int		tx;
-	int		ty;
-	int		checkerBoardPattern;
-	int		floorTexture;
-	int		ceilingTexture;
-}	t_mlx;
+#include "cub3d.h"
 
 int worldMap[MAPW][MAPH]=
 {
