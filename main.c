3d800451550a15f0f0fe9d02f3c5dd2052256085
Frederic Tanon <ftanon@paul-f5Br2s1.clusters.42paris.fr>
2024-09-07 11:23:21 +0200

    core parsing before error management done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ftanon <ftanon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:58:17 by ftanon            #+#    #+#             */
-/*   Updated: 2024/09/07 11:20:36 by ftanon           ###   ########.fr       */
+/*   Updated: 2024/09/07 11:23:13 by ftanon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,13 @@ int	main(int argc, char **argv)
 {
 	t_mlx	*mlx;
 	
-
 	check_argument(argv[1], argc);
 	mlx = malloc(sizeof(t_mlx));
-	// mlx->textures = malloc(6 * sizeof(char *) + 1);
 	open_file(mlx, argv[1]);
 	store_data(mlx);
 	store_textures(mlx);
 	analyse_map(mlx);
 	store_map(mlx);
 	display_parsing(mlx);
-	// if (mlx == NULL)
-		// return (printf("Error: memory allocation for shared data struct failed\n"), 1);
-	
-	// mlx->map_int = parser(argv[1], mlx);
-	// if (mlx->map_int == NULL)
-	// 	return (ender(mlx), 1);
-	// display_parsing(mlx);
-	// ender(mlx);
 	return (0);
 }
