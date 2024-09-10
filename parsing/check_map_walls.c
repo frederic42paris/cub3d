f7789f88a229d1be858cb91b07077d8ded73d8f7
Aether Guez <aet7gz@gmail.com>
2024-09-10 16:46:36 +0200

    refactor: fix norm issues

--- parsing/check_map_walls.c
+++ parsing/check_map_walls.c
@@ -6,18 +6,27 @@
 /*   By: ftanon <ftanon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 17:29:23 by ftanon            #+#    #+#             */
-/*   Updated: 2024/09/10 15:52:11 by arguez           ###   ########.fr       */
+/*   Updated: 2024/09/10 16:45:56 by arguez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
+static int	cmw_x_subroutine(t_mlx *mlx, int x, int y)
+{
+	if (mlx->map_int_one[y][x - 1] != 1)
+	{
+		printf("x error 2  map %d %d\n", y, x - 1);
+		return (1);
+	}
+	return (0);
+}
+
 int	check_map_walls_x(t_mlx *mlx)
 {
-	int	y;
-	int	x;
+	static int	y = 0;
+	int			x;
 
-	y = 0;
 	while (y < mlx->map_width)
 	{
 		x = 0;
@@ -34,23 +43,29 @@ int	check_map_walls_x(t_mlx *mlx)
 				break ;
 			while (x < mlx->map_height && mlx->map_int_one[y][x] != 2)
 				x++;
-			if (mlx->map_int_one[y][x - 1] != 1)
-			{
-				printf("x error 2  map %d %d\n", y, x - 1);
+			if (cmw_x_subroutine(mlx, x, y) == 1)
 				return (1);
-			}
 		}
 		y++;
 	}
 	return (0);
 }
 
+static int	cmw_y_subroutine(t_mlx *mlx, int x, int y)
+{
+	if (mlx->map_int_one[y - 1][x] != 1)
+	{
+		printf("y error 2 map %d %d\n", y - 1, x);
+		return (1);
+	}
+	return (0);
+}
+
 int	check_map_walls_y(t_mlx *mlx)
 {
-	int	y;
-	int	x;
+	int			y;
+	static int	x = 0;
 
-	x = 0;
 	while (x < mlx->map_height)
 	{
 		y = 0;
@@ -67,11 +82,8 @@ int	check_map_walls_y(t_mlx *mlx)
 				break ;
 			while (y < mlx->map_width && mlx->map_int_one[y][x] != 2)
 				y++;
-			if (mlx->map_int_one[y - 1][x] != 1)
-			{
-				printf("y error 2 map %d %d\n", y - 1, x);
+			if (cmw_y_subroutine(mlx, x, y) == 1)
 				return (1);
-			}
 		}
 		x++;
 	}
